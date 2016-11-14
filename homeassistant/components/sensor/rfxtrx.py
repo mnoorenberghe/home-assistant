397ae8129ef26e1f0cee82513bc80ee8a5649c61
Daniel Høyer Iversen <mail@dahoiv.net>
2016-11-14 15:16:12 +0100

    bugfix

--- homeassistant/components/sensor/rfxtrx.py
+++ homeassistant/components/sensor/rfxtrx.py
@@ -48,8 +48,7 @@ def setup_platform(hass, config, add_devices_callback, discovery_info=None):
                     data_types = [data_type]
                     break
         for _data_type in data_types:
-            entity_id = generate_entity_id(ENTITY_ID_FORMAT, device_id,
-                                           hass=hass)
+            entity_id = generate_entity_id(ENTITY_ID_FORMAT, hass=hass)
             new_sensor = RfxtrxSensor(None, entity_info[ATTR_NAME], _data_type,
                                       entity_info[ATTR_FIREEVENT], entity_id)
             sensors.append(new_sensor)
